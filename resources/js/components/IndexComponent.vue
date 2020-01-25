a85aaa8f913008045adb9e7a2a874c6bbe5a3e3f
h-kagata <h-kagata@i-enter.co.jp>
2020-01-25 11:27:01 +0900

    vue.jsのテスト画面

---
+++ resources/js/components/IndexComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        オリンピックアプリ
+    </div>
+</template>
+<script>
+export default {
+    components: {
+    },
+    data() {
+        return {
+        }
+    }
+}
+</script>
